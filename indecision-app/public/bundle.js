9da81f9b8e5af0f1d35a835fd0542de18baab7ed
chonseng <chonseng38@gmail.com>
2018-04-23 23:57:30 -0700

    52 Default Exports

--- indecision-app/public/bundle.js
+++ indecision-app/public/bundle.js
@@ -71,15 +71,17 @@
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__person_js__ = __webpack_require__(2);
 // import './utils.js';
-// import { square, add } from './utils.js';
+// import subtract, { square, add } from './utils.js';
 
 // console.log('app.js is running!');
 // console.log(square(4));
 // console.log(add(100, 23));
+// console.log(subtract(100, 81));
 
 
-console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["b" /* isAdult */](18));
+console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["c" /* isAdult */](18));
 console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["a" /* canDrink */](18));
+console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["b" /* default */](65));
 
 /***/ }),
 /* 1 */,
@@ -88,12 +90,15 @@ console.log(__WEBPACK_IMPORTED_MODULE_0__person_js__["a" /* canDrink */](18));
 
 "use strict";
 const isAdult = (age) => age >= 18;
-/* harmony export (immutable) */ __webpack_exports__["b"] = isAdult;
+/* harmony export (immutable) */ __webpack_exports__["c"] = isAdult;
 
 
 const canDrink = (age) => age >= 21;
 /* harmony export (immutable) */ __webpack_exports__["a"] = canDrink;
 
+
+const isSenior = (age) => age >= 65;
+/* harmony default export */ __webpack_exports__["b"] = (isSenior);
 
 /***/ })
 /******/ ]);
